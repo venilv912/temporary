9829ce3fca6918ddb7b1b2032ef80d97afffb2cf
Venil <venilv912@gmail.com>
2024-11-14 13:59:58 +0530

    pre-installation

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -13,6 +14,8 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+    const __dirname = path.resolve();
 
 const app = express();
 
@@ -25,6 +28,12 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;

7aa9e9a8f83592d3eb677a5986f817145c5b72da
Venil <venilv912@gmail.com>
2024-11-21 23:09:13 +0530

    done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,3 +30,12 @@ export const signin = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signout = async (req, res, next) => {
+    try {
+        res.clearCookie('access_token');
+        res.status(200).json('User has been Logged Out Successfully!');
+    } catch (error) {
+        next(error);
+    }
+};
